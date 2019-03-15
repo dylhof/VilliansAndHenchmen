dbb658443e2f13c8f46cd10fc1e26c75aeb049e1
Dylan Hofmann <dylan.hofmann@gmail.com>
2019-03-15 10:03:01 -0600

    Update to knexfile and procfile

--- knexfile.js
+++ knexfile.js
@@ -13,6 +13,14 @@ module.exports = {
       directory: './db/seeds/development'
     }
   },
+  production: {
+    client: 'pg',
+    connection: process.env.DATABASE_URL + `?ssl=true`,
+    migrations: {
+      directory: './db/migrations'
+    },
+    useNullAsDefault: true
+  }
 
   // staging: {
   //   client: 'postgresql',
@@ -30,20 +38,5 @@ module.exports = {
   //   }
   // },
 
-  // production: {
-  //   client: 'postgresql',
-  //   connection: {
-  //     database: 'my_db',
-  //     user:     'username',
-  //     password: 'password'
-  //   },
-  //   pool: {
-  //     min: 2,
-  //     max: 10
-  //   },
-  //   migrations: {
-  //     tableName: 'knex_migrations'
-  //   }
-  // }
 
 };
