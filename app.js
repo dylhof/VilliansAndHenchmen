1888c948aece9ea1a4c19fbf144eb7a316ca7941
Dylan Hofmann <dylan.hofmann@gmail.com>
2019-03-20 11:35:20 -0600

    Add cors

--- app.js
+++ app.js
@@ -3,13 +3,15 @@ const configuration = require('./knexfile')[environment];
 const database = require('knex')(configuration);
 
 const express = require('express');
-const app = express()
-module.exports = app
-app.use(express.json())
+const app = express();
+const cors = require('cors');
+module.exports = app;
+app.use(express.json());
+app.use(cors());
 
 app.get('/', (req, res) => {
   res.send('Welcome to Disney Villains and Henchmen') 
-})
+});
 
 app.get('/api/v1/villains', (request, response) => {
   database('villains').select()
--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const app = require('./app')
 // import app from './app'
 
-app.set('port', process.env.PORT || 3000);
+app.set('port', process.env.PORT || 3001);
 
 app.listen(app.get('port'), () => {
   console.log(`App is running on port ${app.get('port')}`)
